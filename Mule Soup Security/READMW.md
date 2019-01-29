dcd073de1355aafd92fc4222dc6145589f88486b
JerryRun <565636992@qq.com>
2019-01-29 15:31:26 +0800

    implement dynamic setting Callback Handler`s User and Password

--- Mule Soup Security/READMW.md
+++ Mule Soup Security/READMW.md
@@ -1,7 +1,32 @@
+本文主要梳理Soap Web Service Security. 
+
+多数内容参考[WSS4J官方文档](http://ws.apache.org/wss4j/) 、[Ussing-WSS4J](http://ws.apache.org/wss4j/using.html) 、[CXF-WS-Secutity](http://cxf.apache.org/docs/ws-security.html)、[Mule-Security-Example](https://docs.mulesoft.com/mule-runtime/3.5/soap-web-service-security-example)。
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+## Tip1 How to set Dynamic Password
+
+思路： 在CallbackHandler中维护一个Map属性。 每次调用CXF生成安全的Soap报文前，往这个Map中写入username跟password。
+
+> 参考：
+>
+> [Using Apache WSS4J™ ----Sample Outbound UsernameToken configuration-- ](http://ws.apache.org/wss4j/using.html)
+>
+> [org.apache.cxf.systest.ws.common.UTPasswordCallback](https://gitbox.apache.org/repos/asf?p=cxf.git;a=blob_plain;f=systests/ws-security/src/test/java/org/apache/cxf/systest/ws/common/UTPasswordCallback.java;hb=HEAD)
+>
+> 
 
 
 
--- Mule Soup Security/usclient/src/main/java/com/laurus/service/callback/SetPassword.java
+++ Mule Soup Security/usclient/src/main/java/com/laurus/service/callback/SetPassword.java
@@ -4,18 +4,30 @@
 package com.laurus.service.callback;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.ws.security.WSPasswordCallback;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
 
 /**
  * @author JerryRun
  * 2019年1月29日
  */
-public class SetPassword implements CallbackHandler {
+public class SetPassword implements CallbackHandler,Callable{
+
+    private Map<String, String>passwords=new HashMap<String, String>();
+    
+    public void putPasswords(String username,String password) {
+        this.passwords.put(username, password);
+    }
+
 
     /* (non-Javadoc)
      * @see javax.security.auth.callback.CallbackHandler#handle(javax.security.auth.callback.Callback[])
@@ -25,5 +37,20 @@ public class SetPassword implements CallbackHandler {
         WSPasswordCallback pc=(WSPasswordCallback) callbacks[0];
         pc.setPassword("Jerry's password");
     }
+
+
+    /* 
+     * @see org.mule.api.lifecycle.Callable#onCall(org.mule.api.MuleEventContext)
+     */
+    @Override
+    public Object onCall(MuleEventContext eventContext) throws Exception {
+        String user = eventContext.getMessage().getOutboundProperty("user");
+        String password = eventContext.getMessage().getOutboundProperty("password");
+        this.putPasswords(user, password);
+        return eventContext.getMessage().getPayload();
+    }
+
+    
+
 
 }
